b07e598da0e34b399811ef0852b97749a095cd68
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-09-08 22:38:35 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,24 @@
+
+public class Solution {
+
+    private static final int IMPOSSIBLE_TO_MAKE_SOURCE_NUMBER_EQUAL_TO_TARGET_NUMBER = -1;
+
+    public int minChanges(int sourceNumber, int targetNumber) {
+        int minChangesToMakeSourceNumberEqualToTargetNumber = 0;
+
+        while (sourceNumber > 0 || targetNumber > 0) {
+            int sourceDigit = sourceNumber & 1;
+            int targetDigit = targetNumber & 1;
+
+            if (sourceDigit < targetDigit) {
+                return IMPOSSIBLE_TO_MAKE_SOURCE_NUMBER_EQUAL_TO_TARGET_NUMBER;
+            }
+
+            minChangesToMakeSourceNumberEqualToTargetNumber += sourceDigit ^ targetDigit;
+            sourceNumber >>= 1;
+            targetNumber >>= 1;
+        }
+
+        return minChangesToMakeSourceNumberEqualToTargetNumber;
+    }
+}
