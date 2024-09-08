ccf6a719a4204850df603f2e01ef0a9371c9a609
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-09-08 22:39:06 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,25 @@
+
+/**
+ * @param {number} sourceNumber
+ * @param {number} targetNumber
+ * @return {number}
+ */
+var minChanges = function (sourceNumber, targetNumber) {
+    const IMPOSSIBLE_TO_MAKE_SOURCE_NUMBER_EQUAL_TO_TARGET_NUMBER = -1;
+    let minChangesToMakeSourceNumberEqualToTargetNumber = 0;
+
+    while (sourceNumber > 0 || targetNumber > 0) {
+        const sourceDigit = sourceNumber & 1;
+        const targetDigit = targetNumber & 1;
+
+        if (sourceDigit < targetDigit) {
+            return IMPOSSIBLE_TO_MAKE_SOURCE_NUMBER_EQUAL_TO_TARGET_NUMBER;
+        }
+
+        minChangesToMakeSourceNumberEqualToTargetNumber += sourceDigit ^ targetDigit;
+        sourceNumber >>= 1;
+        targetNumber >>= 1;
+    }
+
+    return minChangesToMakeSourceNumberEqualToTargetNumber;
+};
