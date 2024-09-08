6379d023ae486528b37a4d325a725fe88ed66c56
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-09-08 22:40:20 +0200

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,27 @@
+
+using namespace std;
+
+class Solution {
+
+    static const int IMPOSSIBLE_TO_MAKE_SOURCE_NUMBER_EQUAL_TO_TARGET_NUMBER = -1;
+
+public:
+    int minChanges(int sourceNumber, int targetNumber) const {
+        int minChangesToMakeSourceNumberEqualToTargetNumber = 0;
+
+        while (sourceNumber > 0 || targetNumber > 0) {
+            int sourceDigit = sourceNumber & 1;
+            int targetDigit = targetNumber & 1;
+
+            if (sourceDigit < targetDigit) {
+                return IMPOSSIBLE_TO_MAKE_SOURCE_NUMBER_EQUAL_TO_TARGET_NUMBER;
+            }
+
+            minChangesToMakeSourceNumberEqualToTargetNumber += sourceDigit ^ targetDigit;
+            sourceNumber >>= 1;
+            targetNumber >>= 1;
+        }
+
+        return minChangesToMakeSourceNumberEqualToTargetNumber;
+    }
+};
